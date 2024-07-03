52d583ba9bfb3246e083203e4ef29d19703b9eb2
Yamin39 <mdmufazzalhossainyamin5010@gmail.com>
2024-07-03 11:06:50 +0600

    add: custom cursor

--- src/layout/Root.jsx
+++ src/layout/Root.jsx
@@ -1,5 +1,6 @@
 import AOS from "aos";
 import "aos/dist/aos.css";
+import AnimatedCursor from "react-animated-cursor";
 import { Outlet } from "react-router-dom";
 import heroShape from "../assets/images/hero-shape.webp";
 import Navbar from "../components/shared/Navbar/Navbar";
@@ -13,6 +14,26 @@ const Root = () => {
       <div className="relative">
         <Navbar></Navbar>
         <Outlet></Outlet>
+      </div>
+
+      {/* custom cursor */}
+      <div className={window.innerWidth >= 768 ? "block" : "hidden"}>
+        <AnimatedCursor
+          innerSize={8}
+          outerSize={35}
+          innerScale={1}
+          outerScale={2}
+          outerAlpha={0}
+          hasBlendMode={true}
+          // showSystemCursor={true}
+          showSystemCursor={window.innerWidth >= 768 ? false : true}
+          innerStyle={{
+            backgroundColor: "black",
+          }}
+          outerStyle={{
+            border: "0.1875rem solid black",
+          }}
+        />
       </div>
     </div>
   );
